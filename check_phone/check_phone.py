042d2a600404b51252dc432846bc0305a95f25aa
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-07-10 08:19:29 -0300

    Add env vars into CHECKS dict

--- check_phone/check_phone.py
+++ check_phone/check_phone.py
@@ -3,6 +3,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=Path('.', '.env'))
 
-for name, value in os.environ.items():
-    if name.split('_')[0] == 'CHECK':
-    	print("{0}: {1}".format(name, value))
+CHECKS = {}
+
+for key, value in os.environ.items():
+  if key.split('_')[0] == 'CHECK':
+    cpf = value.split(',')[0]
+    companies = value.split(',')[1:]
+    name = ' '.join(key.split('_')[1:])        
+    CHECKS[f'{cpf}'] = {
+        'name': name,
+        'companies': companies,    
+    }
